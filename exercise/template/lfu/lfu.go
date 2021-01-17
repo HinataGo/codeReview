8c0cd23433f73a811241623aec0b369280c631f3
HinataGo <365264652@qq.com>
2021-01-17 10:50:55 +0800

    lfu

--- exercise/template/lfu/lfu.go
+++ exercise/template/lfu/lfu.go
@@ -1,17 +1,16 @@
 package lfu
 
-// TODO
-// type LFUCache struct {
-// }
-//
-// func Constructor(capacity int) LFUCache {
-//
-// }
-//
-// func (this *LFUCache) Get(key int) int {
-//
-// }
-//
-// func (this *LFUCache) Put(key int, value int) {
-//
-// }
+type LFUCache struct {
+}
+
+func Constructor(capacity int) LFUCache {
+
+}
+
+func (this *LFUCache) Get(key int) int {
+
+}
+
+func (this *LFUCache) Put(key int, value int) {
+
+}
