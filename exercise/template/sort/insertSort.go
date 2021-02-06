4067690c4b7b022e90373fa9d221eff1dbe6c10e
HinataGo <365264652@qq.com>
2021-02-06 23:11:51 +0800

    fix

--- exercise/template/sort/insertSort.go
+++ exercise/template/sort/insertSort.go
@@ -1,15 +1,24 @@
 package sort
 
 // 将第一待排序序列第一个元素看做一个有序序列，把第二个元素到最后一个元素当成是未排序序列。
-// 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。（如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
-
+// 从头到尾依次扫描未排序序列，将扫描到的每个元素插入有序序列的适当位置。
+// （如果待插入的元素与有序序列中的某个元素相等，则将待插入元素插入到相等元素的后面。）
+/*
+从第一个元素开始，该元素可以认为已经被排序；
+取出下一个元素，在已经排序的元素序列中从后向前扫描；
+如果该元素（已排序）大于新元素，将该元素移到下一位置；
+重复步骤3，直到找到已排序的元素小于或者等于新元素的位置；
+将新元素插入到该位置后；
+重复步骤2~5
+*/
 func insertionSort(arr []int) []int {
+	preIndex, current := 0, 0
 	for i := range arr {
-		preIndex := i - 1
-		current := arr[i]
+		preIndex = i - 1
+		current = arr[i]
 		for preIndex >= 0 && arr[preIndex] > current {
 			arr[preIndex+1] = arr[preIndex]
-			preIndex -= 1
+			preIndex--
 		}
 		arr[preIndex+1] = current
 	}
