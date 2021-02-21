65f4239f941b44a230d5b133058a69b35792bcdc
HinataGo <365264652@qq.com>
2021-02-21 20:41:32 +0800

    select

---
+++ go/go_api/concurrency/select/select_test.go
@@ -0,0 +1,32 @@
+package _select
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+// select是专门针对channel的一种语句
+func service() string {
+	time.Sleep(time.Millisecond * 500)
+	return "Done"
+}
+
+func AsyncService() chan string {
+	retCh := make(chan string, 1)
+	go func() {
+		ret := service()
+		fmt.Println("returned result.")
+		retCh <- ret
+	}()
+	return retCh
+}
+
+func TestSelect(t *testing.T) {
+	select {
+	case ret := <-AsyncService():
+		t.Log(ret)
+	case <-time.After(time.Millisecond * 100):
+		t.Error("time out")
+	}
+}
