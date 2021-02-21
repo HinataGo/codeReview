dcc7f5afc831cd95a4b46caec83fb44ddb866187
HinataGo <365264652@qq.com>
2021-02-21 20:53:58 +0800

    单元测试

---
+++ go/go_api/test/unit_test.go
@@ -0,0 +1,48 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSquare(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		if ret != expected[i] {
+			t.Errorf("input is %d, the expected is %d, but the actual is %d",
+				inputs[i], expected[i], ret)
+		}
+	}
+}
+
+func TestErrorInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Error("Error")
+	fmt.Println("End")
+}
+
+func TestFatalInCode(t *testing.T) {
+	fmt.Println("Start")
+	t.Fatal("Fatal")
+	fmt.Println("End")
+}
+
+func TestSquareAssert(t *testing.T) {
+	inputs := [...]int{1, 2, 3}
+	expected := [...]int{1, 4, 9}
+	for i := 0; i < len(inputs); i++ {
+		ret := square(inputs[i])
+		assert.Equal(t, expected[i], ret)
+	}
+}
+
+func square(op int) int {
+	if op == 10 {
+		fmt.Println(10)
+	}
+	return op * op
+}
